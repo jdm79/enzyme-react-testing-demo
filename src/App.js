229d8e9eca2fbf4546c6ee6b707a21cefdce91f9
jdm79 <james.malvern@gmail.com>
2018-11-16 19:00:58 +0000

    deletes the buttons and functions to start again test first

--- src/App.js
+++ src/App.js
@@ -2,35 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: "The count is: ",
-      count: 0
-    }
-  }
-
-  incrementCount = () => {
-    this.setState({count: this.state.count +1 })
-  }
-
-  decrementCount = () => {
-    this.setState({count: this.state.count -1 })
-  }
-
-  resetCount = () => {
-    this.setState({count: 0})
-  }
-
 
   render() {
     return (
       <div className="App">
         <h1>{this.props.title}</h1>
-        <h2>{this.state.message} {this.state.count}</h2>
-        <button onClick={this.incrementCount}>Increment Counter</button>
-        <button onClick={this.resetCount}>Reset Counter</button>
-        <button onClick={this.decrementCount}>Decrement Counter</button>
+      
       </div>
     );
   }
