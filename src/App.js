c064988ca4bb7186c04f51102b30461fa7984cfc
jdm79 <james.malvern@gmail.com>
2018-11-16 19:27:48 +0000

    adds a button which renders, passes test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App" data-test="component-app">
+        <button data-test="increment-button">Increment Counter</button>
       
       </div>
     );
