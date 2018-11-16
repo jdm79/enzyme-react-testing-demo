282da0b3d1a265b3557d2bfc6496cf9a59526147
jdm79 <james.malvern@gmail.com>
2018-11-16 20:39:57 +0000

    adds an increment counter function which updates the state of the counter by 1 when clicked, test passes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       counter: 0
     }
   }
 
+  incrementCounter = () => {
+    this.setState({ counter: this.state.counter +1})
+  }
+
   render() {
     return (
       <div className="App" data-test="component-app">
-        <h1 data-test="counter-display">The counter is currently: </h1>
-        <button data-test="increment-button">Increment Counter</button>
+        <h1 data-test="counter-display">The counter is currently: {this.state.counter}</h1>
+        <button data-test="increment-button" onClick={this.incrementCounter}>Increment Counter</button>
       </div>
     );
   }
