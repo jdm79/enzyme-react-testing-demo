8d4343ec1e3f6e495a4c4579efdaa82697faa647
jdm79 <james.malvern@gmail.com>
2018-11-16 20:22:26 +0000

    adds constructor and state, sets counter to 0

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      counter: 0
+    }
+  }
 
   render() {
     return (
       <div className="App" data-test="component-app">
-        <h1 data-test="counter-display"></h1>
+        <h1 data-test="counter-display">The counter is currently: </h1>
         <button data-test="increment-button">Increment Counter</button>
       </div>
     );
