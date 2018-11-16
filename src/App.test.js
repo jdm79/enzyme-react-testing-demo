c5ff3c67b764f45ef9b502aa6dfb9af563b336eb
jdm79 <james.malvern@gmail.com>
2018-11-16 20:26:47 +0000

    refactors #setup to deal with changing state when testing the increment button

--- src/App.test.js
+++ src/App.test.js
@@ -9,11 +9,13 @@ Enzyme.configure({ adapter: new EnzymeAdapter() });
  * Factory function to create a ShallowWrapper for the App component.
  * @function setup
  * @param {object} props - Component props specific to this setup.
- * @param {any} state - Initial state for setup
+ * @param {object} state - Initial state for setup
  * @returns {ShallowWrapper}
  */
 const setup = (props ={}, state=null) => {
-  return shallow(<App {...props} />)
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper
 }
 
 /**
