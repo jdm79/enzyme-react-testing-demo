2b7f595a138c878f0bd6062da9cc33328aa882a7
jdm79 <james.malvern@gmail.com>
2018-11-16 16:27:03 +0000

    imports #shallow from Enzyme, renders App component in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Enzyme from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test('it renders without crashing', () => {
-
+  const wrapper = shallow(<App />);
 });
