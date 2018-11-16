324e7a1ca398a0f6223e3f56c4230c65ce8c9b3c
jdm79 <james.malvern@gmail.com>
2018-11-16 19:36:42 +0000

    adds h1 for counter display

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App" data-test="component-app">
+        <h1 data-test="counter-display"></h1>
         <button data-test="increment-button">Increment Counter</button>
-      
       </div>
     );
   }
