4c3c420c4d1a22a9f4ff9640dc0844420d654a96
jdm79 <james.malvern@gmail.com>
2018-11-16 19:22:40 +0000

    writes test for rendering something on the div without error

--- src/App.test.js
+++ src/App.test.js
@@ -6,9 +6,26 @@ import App from './App';
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 test('renders without error', () => {
-
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find('[data-test="component-app"]');
+  expect(appComponent.length).toBe(1)
 })
 
 test('renders increment button', () => {
-  
+
+})
+
+test('displays our counter', () => {
+
+})
+
+// this is testing state, but meh for now
+test('counter starts at 0', () => {
+
+})
+
+// testing that it affects the display is better directly checking the state
+// less about the implementation, more about the behaviour
+test('clicking button increments counter display', () => {
+
 })
